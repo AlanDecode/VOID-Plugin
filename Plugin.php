10fe55fc70f11c6b0ec9df5e224a7a5b88c7580e
AlanDecode <zytbuaa1415@gmail.com>
2019-04-27 18:41:09 +0800

    add 浏览量统计

--- Plugin.php
+++ Plugin.php
@@ -44,6 +44,19 @@ class VOID_Plugin implements Typecho_Plugin_Interface
         if (!array_key_exists('likes', $db->fetchRow($db->select()->from('table.contents'))))
             $db->query('ALTER TABLE `'. $prefix .'contents` ADD `likes` INT(10) DEFAULT 0;');
         Helper::addAction('void_like', 'VOID_Action');
+
+        /** 浏览量统计相关 */
+        // 创建字段
+        if (!array_key_exists('viewsNum', $db->fetchRow($db->select()->from('table.contents'))))
+            $db->query('ALTER TABLE `'. $prefix .'contents` ADD `viewsNum` INT(10) DEFAULT 0;');
+        // 删除以前创建的沙雕字段
+        if (array_key_exists('views', $db->fetchRow($db->select()->from('table.contents'))))
+            $db->query('ALTER TABLE `'. $prefix .'contents` DROP `views`;');
+        //增加浏览数
+        Typecho_Plugin::factory('Widget_Archive')->beforeRender = array('VOID_Plugin', 'updateViewCount');
+
+        // 将添加的字段加入查询
+        Typecho_Plugin::factory('Widget_Archive')->select = array('VOID_Plugin', 'selectHandle');
     }
 
     /**
@@ -81,6 +94,33 @@ class VOID_Plugin implements Typecho_Plugin_Interface
     public static function personalConfig(Typecho_Widget_Helper_Form $form){}
 
     /**
+     * 自定义字段加入查询
+     */
+    public static function selectHandle($archive){
+        $user = Typecho_Widget::widget('Widget_User');
+        if ('post' == $archive->parameter->type || 'page' == $archive->parameter->type) {
+            if ($user->hasLogin()) {
+                $select = $archive->select()->where('table.contents.status = ? OR table.contents.status = ? OR
+                        (table.contents.status = ? AND table.contents.authorId = ?)',
+                        'publish', 'hidden', 'private', $user->uid);
+            } else {
+                $select = $archive->select()->where('table.contents.status = ? OR table.contents.status = ?',
+                        'publish', 'hidden');
+            }
+        } else {
+            if ($user->hasLogin()) {
+                $select = $archive->select()->where('table.contents.status = ? OR
+                        (table.contents.status = ? AND table.contents.authorId = ?)', 'publish', 'private', $user->uid);
+            } else {
+                $select = $archive->select()->where('table.contents.status = ?', 'publish');
+            }
+        }
+        $select->where('table.contents.created < ?', Typecho_Date::gmtTime());
+        $select->cleanAttribute('fields');
+        return $select;
+    }
+
+    /**
      * 更新文章字数统计
      * 
      * @access public
@@ -90,5 +130,35 @@ class VOID_Plugin implements Typecho_Plugin_Interface
     public static function updateWordCount($contents, $widget)
     {
         VOID_WordCount::wordCountByCid($widget->cid);
+    }
+
+    /**
+     * 更新文章浏览量
+     * 
+     * @param Widget_Archive   $archive
+     * @return void
+     */
+    public static function updateViewCount($archive) {
+        if($archive->is('single')){
+            $cid = $archive->cid;
+            $views = Typecho_Cookie::get('__void_post_views');
+            if(empty($views)){
+                $views = array();
+            } else {
+                $views = explode(',', $views);
+            }
+            if(!in_array($cid,$views)){
+                $db = Typecho_Db::get();
+                $row = $db->fetchRow($db->select('viewsNum')
+                    ->from('table.contents')
+                    ->where('cid = ?', $cid));
+                $db->query($db->update('table.contents')
+                    ->rows(array('viewsNum' => (int)$row['viewsNum']+1))
+                    ->where('cid = ?', $cid));
+                array_push($views, $cid);
+                $views = implode(',', $views);
+                Typecho_Cookie::set('__void_post_views', $views); //记录查看cookie
+            }
+        }
     }
 }
