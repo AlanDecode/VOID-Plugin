22e86ff7bf6d7c117babd3e9dca2daa0d2e58734
AlanDecode <zytbuaa1415@gmail.com>
2019-10-05 09:48:23 +0800

    refactor

--- Plugin.php
+++ Plugin.php
@@ -17,6 +17,31 @@ class VOID_Plugin implements Typecho_Plugin_Interface
 {
     public static $VERSION = 1.2;
 
+    private static function hasColumn($table, $field) {
+        $db = Typecho_Db::get();
+
+        $row = $db->fetchRow($db->select('count(*)')->from('information_schema.columns')
+            ->where('table_name = ?', $table)
+            ->where('column_name = ?', $field));
+
+        return boolval($row['count(*)']);
+    }
+
+    private static function hasTable($table) {
+        $db = Typecho_Db::get();
+        $sql = "SHOW TABLES LIKE '%".$table."%'";
+        return count($db->fetchAll($sql)) != 0;
+    }
+
+    private static function queryAndCatch($sql) {
+        $db = Typecho_Db::get();
+        try {
+            $db->query($sql);
+        } catch (Typecho_Db_Query_Exception $th) {
+            throw new Typecho_Plugin_Exception($th->getMessage());
+        }
+    }
+
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
      * 
@@ -28,6 +53,7 @@ class VOID_Plugin implements Typecho_Plugin_Interface
     {
         // 检查数据库类型
         $db = Typecho_Db::get();
+        $prefix = $db->getPrefix();
         $adapterName =  strtolower($db->getAdapterName());
         if (strpos($adapterName, 'mysql') < 0) {
             throw new Typecho_Plugin_Exception('启用失败，本插件暂时只支持 MySQL 数据库，您的数据库是：'.$adapterName);
@@ -35,81 +61,78 @@ class VOID_Plugin implements Typecho_Plugin_Interface
 
         // 检查是否存在对应扩展
         if (!extension_loaded('openssl')) {
-            throw new Typecho_Plugin_Exception('启用失败，PHP 未启用 OpenSSL 扩展。');
+            throw new Typecho_Plugin_Exception('启用失败，PHP 需启用 OpenSSL 扩展。');
         }
-
-        $prefix = $db->getPrefix();
         
-        /** 字数统计相关 */
+        /** 字数统计 */
         // contents 表中若无 wordCount 字段则添加
-        if (!array_key_exists('wordCount', $db->fetchRow($db->select()->from('table.contents'))))
-            $db->query('ALTER TABLE `'. $prefix .'contents` ADD COLUMN `wordCount` INT(10) DEFAULT 0;');
+        if (!self::hasColumn($prefix.'contents', 'wordCount')) {
+            self::queryAndCatch('ALTER TABLE `'. $prefix .'contents` ADD COLUMN `wordCount` INT(10) DEFAULT 0;');
+        }
         // 更新一次字数统计
         VOID_WordCount::updateAllWordCount();
         // 注册 hook
         Typecho_Plugin::factory('Widget_Contents_Post_Edit')->finishPublish = array('VOID_Plugin', 'updateContent');
         Typecho_Plugin::factory('Widget_Contents_Page_Edit')->finishPublish = array('VOID_Plugin', 'updateContent');
+        // 加入查询
+        Typecho_Plugin::factory('Widget_Archive')->___wordCount = array('VOID_Plugin', 'wordCount');
 
-        /** 点赞相关 */
+        /** 文章点赞 */
         // 创建字段
-        if (!array_key_exists('likes', $db->fetchRow($db->select()->from('table.contents'))))
-            $db->query('ALTER TABLE `'. $prefix .'contents` ADD COLUMN `likes` INT(10) DEFAULT 0;');
+        if (!self::hasColumn($prefix.'contents', 'likes')) {
+            self::queryAndCatch('ALTER TABLE `'. $prefix .'contents` ADD COLUMN `likes` INT(10) DEFAULT 0;');
+        }
+        // 加入查询
+        Typecho_Plugin::factory('Widget_Archive')->___likes = array('VOID_Plugin', 'likes');
         
         /** 评论赞踩 */
         // 创建字段
-        if (!array_key_exists('likes', $db->fetchRow($db->select()->from('table.comments'))))
-            $db->query('ALTER TABLE `'. $prefix .'comments` ADD COLUMN `likes` INT(10) DEFAULT 0;');
-        if (!array_key_exists('dislikes', $db->fetchRow($db->select()->from('table.comments'))))
-            $db->query('ALTER TABLE `'. $prefix .'comments` ADD COLUMN `dislikes` INT(10) DEFAULT 0;');
-
-        /** 浏览量统计相关 */
-        // 创建字段
-        if (!array_key_exists('viewsNum', $db->fetchRow($db->select()->from('table.contents'))))
-            $db->query('ALTER TABLE `'. $prefix .'contents` ADD COLUMN `viewsNum` INT(10) DEFAULT 0;');
-        //增加浏览数
-        Typecho_Plugin::factory('Widget_Archive')->beforeRender = array('VOID_Plugin', 'updateViewCount');
-
-        // 增加查询方法
-        Typecho_Plugin::factory('Widget_Archive')->___viewsNum = array('VOID_Plugin', 'viewsNum');
-        Typecho_Plugin::factory('Widget_Archive')->___likes = array('VOID_Plugin', 'likes');
-        Typecho_Plugin::factory('Widget_Archive')->___wordCount = array('VOID_Plugin', 'wordCount');
-
-        // 创建表，保存点赞与投票相关信息
-        $table_name = $prefix . 'votes';
-        $sql = "SHOW TABLES LIKE '%" . $table_name . "%'";
-
-        try {
-            if (count($db->fetchAll($sql)) == 0) {
-                $sql = 'create table `'.$table_name.'` (
-                    `vid` int unsigned auto_increment,
-                    `id` int unsigned not null,
-                    `table` char(32) not null,
-                    `type` char(32) not null,
-                    `agent` text,
-                    `ip` varchar(128),
-                    `created` int unsigned default 0,
-                    primary key (`vid`)
-                ) default charset=utf8;
-                CREATE INDEX index_ip ON '.$table_name.'(`ip`);
-                CREATE INDEX index_id ON '.$table_name.'(`id`);
-                CREATE INDEX index_table ON '.$table_name.'(`table`)';
-
-                $sqls = explode(';', $sql);
-                foreach ($sqls as $sql) {
-                    $db->query($sql);
-                }
-            } else {
-                if (!array_key_exists('created', $db->fetchRow($db->select()->from('table.votes')))) {
-                    $db->query('ALTER TABLE `'. $prefix .'votes` ADD COLUMN `created` INT(10) DEFAULT 0;');
-                }
-            }
-        } catch (Typecho_Db_Query_Exception $th) {
-            throw new Typecho_Plugin_Exception($th->getMessage());
+        if (!self::hasColumn($prefix.'comments', 'likes')) {
+            self::queryAndCatch('ALTER TABLE `'. $prefix .'comments` ADD COLUMN `likes` INT(10) DEFAULT 0;');
+        }
+        if (!self::hasColumn($prefix.'comments', 'dislikes')) {
+            self::queryAndCatch('ALTER TABLE `'. $prefix .'comments` ADD COLUMN `dislikes` INT(10) DEFAULT 0;');
         }
 
-        /**
-         * 添加一个面板，展示互动信息，例如评论赞踩、文章点赞
-         */
+        /** 浏览量统计 */
+        // 创建字段
+        if (!self::hasColumn($prefix.'contents', 'viewsNum')) {
+            self::queryAndCatch('ALTER TABLE `'. $prefix .'contents` ADD COLUMN `viewsNum` INT(10) DEFAULT 0;');
+        }
+        //增加浏览数
+        Typecho_Plugin::factory('Widget_Archive')->beforeRender = array('VOID_Plugin', 'updateViewCount');
+        // 加入查询
+        Typecho_Plugin::factory('Widget_Archive')->___viewsNum = array('VOID_Plugin', 'viewsNum');
+
+        /** 点赞与投票数据库 */
+        // 创建表，保存点赞与投票相关信息
+        $table_name = $prefix . 'votes';
+        if (!self::hasTable($table_name)) {
+            $sql = 'create table IF NOT EXISTS `'.$table_name.'` (
+                `vid` int unsigned auto_increment,
+                `id` int unsigned not null,
+                `table` char(32) not null,
+                `type` char(32) not null,
+                `agent` text,
+                `ip` varchar(128),
+                `created` int unsigned default 0,
+                primary key (`vid`)
+            ) default charset=utf8;
+            CREATE INDEX index_ip ON '.$table_name.'(`ip`);
+            CREATE INDEX index_id ON '.$table_name.'(`id`);
+            CREATE INDEX index_table ON '.$table_name.'(`table`)';
+
+            $sqls = explode(';', $sql);
+            foreach ($sqls as $sql) {
+                self::queryAndCatch($sql);
+            }
+        } else {
+            if (!self::hasColumn($prefix.'votes', 'created')) {
+                self::queryAndCatch('ALTER TABLE `'. $prefix .'votes` ADD COLUMN `created` INT(10) DEFAULT 0;');
+            }
+        }
+
+        // 添加一个面板，展示互动信息，例如评论赞踩、文章点赞
         Helper::addPanel(3, 'VOID/pages/showActivity.php', '互动', '查看访客互动', 'administrator');
 
         // 添加投票路由，文章与评论
@@ -130,6 +153,7 @@ class VOID_Plugin implements Typecho_Plugin_Interface
     public static function deactivate()
 	{
         Helper::removeAction('void');
+        Helper::removeAction('void_vote');
         Helper::removePanel(3, 'VOID/pages/showActivity.php');
     }
     
